9c7c0c4d9f248712ac4f0e1f100cf4838f1186d9
ArtKonovalow <artkonovalow@gmail.com>
2020-05-05 19:02:00 +0300

    make min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
-gulp.task('hellow', function() {
-   console.log('Привет, мир!');
-   document();
+gulp.task('hellow', function () {
+    console.log('Привет, мир!');
+    document();
 });
 
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.watch("./*.html").on('change', browserSync.reload);
+
+gulp.task('cssMin', function () {
+    gulp.src(['css/*.css', '!css/*.min.css'])
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('css'));
 });
