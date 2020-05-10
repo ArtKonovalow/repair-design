13bd975061aa6ebbf39caf93519ba421165587ac
ArtKonovalow <artkonovalow@gmail.com>
2020-05-10 13:28:16 +0300

    feat: add Futura book & Demi fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 // Static server
 function bs() {
@@ -20,6 +21,9 @@ watch("./js/*.js").on('change', browserSync.reload);
 function serveSass() {
     return src("./sass/*.sass")
         .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
 };
