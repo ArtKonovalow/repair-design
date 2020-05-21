16fb28dfc2521f684f0c85e036d8ed476c8293e2
ArtKonovalow <artkonovalow@gmail.com>
2020-05-21 13:12:21 +0300

    feat: add button up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) {
    const modal = document.querySelector('.modal');
    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -12,3 +13,44 @@ document.addEventListener("DOMContentLoaded", function(event) {
    closeBtn.addEventListener('click', switchModal);
 
 });
+*/
+
+/*модальное окно*/
+$(document).ready(function () {
+   var modal = $('.modal'),
+      modalBtn = $('[data-toggle="modal"]'),
+      closeBtn = $('.modal__close');
+
+   modalBtn.on('click', function () {
+      modal.toggleClass('modal--visible');
+   });
+   closeBtn.on('click', function () {
+      modal.toggleClass('modal--visible');
+   });
+});
+
+/* 
+$(document).keydown(function(e) {
+   if (e.keyCode == 27) {
+      modal.toggleClass('modal--visible');
+   }
+});
+*/
+
+/* Кнопка вверх*/
+$(document).ready(function() { 
+   var button = $('#upbutton');	
+   $(window).scroll (function () {
+     if ($(this).scrollTop () > 300) {
+       button.fadeIn();
+     } else {
+       button.fadeOut();
+     }
+ });	 
+ button.on('click', function(){
+ $('body, html').animate({
+ scrollTop: 0
+ }, 800);
+ return false;
+ });		 
+ });
