7c09620ed91c86dedc1110abf13f5f411dc4af74
ArtKonovalow <artkonovalow@gmail.com>
2020-06-16 17:55:15 +0300

    feat: add map

--- js/main.js
+++ js/main.js
@@ -174,5 +174,37 @@ $(document).ready(function() {
  scrollTop: 0
  }, 800);
  return false;
- });		 
  });
+ 
+ // yandex map
+ ymaps.ready(function () {
+   var myMap = new ymaps.Map('map', {
+           center: [56.845872, 35.905525],
+           zoom: 9
+       }, {
+           searchControlProvider: 'yandex#search'
+       }),
+
+       // Создаём макет содержимого.
+       MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+           '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+       ),
+
+       myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+           hintContent: 'Наш офис',
+       }, {
+           // Опции.
+           // Необходимо указать данный тип макета.
+           iconLayout: 'default#image',
+           // Своё изображение иконки метки.
+           iconImageHref: 'img/marker.png',
+           // Размеры метки.
+           iconImageSize: [32, 32],
+           // Смещение левого верхнего угла иконки относительно
+           // её "ножки" (точки привязки).
+           iconImageOffset: [-5, -38]
+       });
+   myMap.geoObjects
+       .add(myPlacemark)
+   });
+});
